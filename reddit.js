d015fe48c9934e738cf0373b1b42fed423044b04
JDSlimz <joshuagreen118@gmail.com>
2015-07-28 00:14:58 -0400

    possibru?

--- reddit.js
+++ reddit.js
@@ -39,7 +39,7 @@
 					base = "r/" + subreddit
 				}
 
-				var embedUrl = host + '/' + base + '/' + sort + '?cssTheme=' + cssTheme + '&embedId=' + embedId + '#' + grid
+				var embedUrl = 'http://redditjs.com/subreddit.js' + '/' + base + '/' + sort + '?cssTheme=' + cssTheme + '&embedId=' + embedId + '#' + grid
 
 				var iframeWrapper = document.createElement("div");
 				iframeWrapper.style.width = '100%'
